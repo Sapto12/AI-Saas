4099b54babfe82a01243301e394b58e998172578
Sapto12 <sappppzy@gmail.com>
2025-12-05 22:11:45 +0700

    feat: enhance Hero component with navigation and add AiTools component to home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 const home = () => {
   return (
@@ -8,6 +9,7 @@ const home = () => {
      
     <NavBar />
     <Hero />
+    <AiTools />
 
     </>
   )
